0635dfec826d9128eea26419d5767b7501a93b5d
Sakurakdx <1441561897@qq.com>
2020-11-05 22:19:00 +0800

    create list

--- create_list.py
+++ create_list.py
@@ -5,21 +5,9 @@ class create_list(object):
     def __init__(self, x):
         self.x = x  # 句子集合
         self.word_list = []  # 分词后的结果
-        self.vocab = []  # 词表[词1， 词2， ...... ， 词n]
+        self.vocab = [0]  # 词表[词1， 词2， ...... ， 词n]  互斥
         self.word2idx = dict()  # 词->id的映射
-        self.vocab_size = 0
-
-    def participle(self):
-        """
-        对x内的句子进行分词
-        :return:
-        """
-        a = []
-        for sen in self.x:
-            sen_list = jieba.lcut(sen, cut_all=False)
-            a.extend(sen_list)
-
-        return a
+        self.length = []  # 句子长度
 
     def cre_list(self):
         """
@@ -27,7 +15,31 @@ class create_list(object):
         :param a:
         :return:
         """
-        self.word_list = self.participle()
+        self.participle()
         self.vocab = list(set(self.word_list))
-        self.word2idx = {w: (i+1) for i, w in enumerate(self.vocab)}
+        self.word2idx = {w: (i + 1) for i, w in enumerate(self.vocab)}
+        self.count_length()
         print("构建词表成功")
+
+    def participle(self):
+        """
+        对x内的句子进行分词,改变word_list,返回分词后的句子集合
+        :return:a：[["词11", "词12", ...], ["词21", "词22", ...], ..., ["词n1", "词n2", ...]]
+        """
+        a = []
+        for sen in self.x:
+            sen_list = jieba.lcut(sen, cut_all=False)
+            self.word_list.extend(sen_list)
+            a.append(sen_list)
+
+        return a
+
+    def count_length(self):
+        """
+
+        :return:
+        """
+        a = self.participle()  # 分词，a = [["词11", "词12", ...], ["词21", "词22", ...], ..., ["词n1", "词n2", ...]]
+        for sen in a:
+            length = len(sen)
+            self.length.append(length)
